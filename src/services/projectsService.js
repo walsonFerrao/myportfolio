52311277923bdcc019ed65764319e0dc17a60d20
ashish bhagat <ashish.bhagat@grappus.com>
2022-03-20 16:09:37 +0530

    fixes

--- src/services/projectsService.js
+++ src/services/projectsService.js
@@ -1,28 +1,28 @@
+import dineout1 from '../assets/images/dineout1.png';
+import dineout2 from '../assets/images/dineout2.png';
+
 export const projects = [
 	{
 		title: 'Levi’s Clone',
-		description:
-			"Levi’s.com is an e-commerce website that sells clothing of the Levi brand.",
-           
-	one:"Multi-functional filter",
-	two:"Cart and payment sections",
-    three:"Search and Debouncing feature",
-	four:"Product display carousel",
-	               
+		description: 'Levi’s.com is an e-commerce website that sells clothing of the Levi brand.',
 
-		technologiesUsed: [
-			'Node',
-			'Express',
-			'MongoDB',
-			'Mongoose',
-			'JavaScript',
-			'CSS',
+		one: 'Multi-functional filter',
+		two: 'Cart and payment sections',
+		three: 'Search and Debouncing feature',
+		four: 'Product display carousel',
+
+		technologiesUsed: ['Node', 'Express', 'MongoDB', 'Mongoose', 'JavaScript', 'CSS'],
+
+		image: [
+			{
+				link: dineout1,
+				index: 1,
+			},
+			{
+				index: 2,
+				link: dineout2,
+			},
 		],
-
-		image: {
-			name: 'memories-mockup',
-			alt: 'laptop and mobile mockup of Memories app',
-		},
 
 		websiteLink: 'https://memories-app-mushfiq.netlify.app',
 		githubLink: 'https://github.com/Ayon95/fullstack-memories-app',
@@ -32,25 +32,23 @@ export const projects = [
 		title: 'Dineout Clone',
 		description:
 			'An appointment-management app that allows users to add, edit, or delete their appointments. Doctors can store and manage patient information such as name, age, phone number, and appointment date.',
-			one:"Sign-in/Sign-up",
-			two:"Sort and filter",
-			three:"Payment page integration",
-			four:"Responsive website",
+		one: 'Sign-in/Sign-up',
+		two: 'Sort and filter',
+		three: 'Payment page integration',
+		four: 'Responsive website',
 		technologiesUsed: ['JavaScript', 'HTML', 'CSS'],
-		image: {
-			name: 'easy-appoint-mockup',
-			alt: 'laptop and mobile mockup of EasyAppoint app',
-		},
+		image: [
+			{
+				link: dineout1,
+				index: 1,
+			},
+			{
+				index: 2,
+				link: dineout2,
+			},
+		],
 
 		websiteLink: 'https://easy-appoint.netlify.app',
 		githubLink: 'https://github.com/Ayon95/easy-appoint',
-	}
-
-	
-
-	
-
-	
-
-	
+	},
 ];
